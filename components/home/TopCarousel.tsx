4303b58956d199d33b63fd386b0b92bf2ef6f0f4
abhishek232005 <abhishek232005@gmail.com>
2025-11-18 19:53:34 +0530

    updated project code

--- components/home/TopCarousel.tsx
+++ components/home/TopCarousel.tsx
@@ -48,7 +48,7 @@ export default function TopCarousel(): React.JSX.Element {
       setLoading(false);
     }
   };
-
+ console.log("API URL = ", process.env.NEXT_PUBLIC_API_URL);
   fetchBanners();
 }, []);
 
